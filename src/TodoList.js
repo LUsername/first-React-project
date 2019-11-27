1d0117b82d081c5f24e6a2da3420a1b223379d70
LUsername <492254446@qq.com>
2019-11-27 20:25:09 +0800

    更多 JSX 语法细节（1）

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,13 @@
 import React,{Component,Fragment} from 'react';
+import './style.css';
 
 class TodoList extends Component{
     // constructor 在组件创建的第一个时刻自动被执行
     constructor(props){
         super(props);
+        // 优化一：避免因事件触发，同一函数多次生成
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleKeyUp = this.handleKeyUp.bind(this);
         // 在组件中创建了两个数据，数据一定要定义在state中
         this.state = {
             inputValue:'',
@@ -33,25 +37,26 @@ class TodoList extends Component{
         this.setState({list});
     }
 
+    // 优化二
+    getListItems(){
+        return this.state.list.map((value,index)=>{
+            return (
+            <li 
+                key={index}
+                onClick={this.handleItemClick.bind(this,index)}
+            >{value}</li>)
+        });
+    }
+
     render(){
         return(
             <Fragment>
-                <input 
+                <input className='input'
                     value={this.state.inputValue}
-                    onChange={this.handleInputChange.bind(this)}
-                    onKeyUp={this.handleKeyUp.bind(this)}
+                    onChange={this.handleInputChange}
+                    onKeyUp={this.handleKeyUp}
                 ></input>
-                <ul>
-                    {
-                        this.state.list.map((value,index)=>{
-                            return (
-                            <li 
-                                key={index}
-                                onClick={this.handleItemClick.bind(this,index)}
-                            >{value}</li>)
-                        })
-                    }
-                </ul>
+                <ul>{this.getListItems()}</ul>
             </Fragment>
         )
     }
