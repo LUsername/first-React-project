dfef28c69b0026123b9e8c3039e137be26a47813
LUsername <492254446@qq.com>
2019-11-27 19:27:49 +0800

    React 中的事件绑定

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,18 +1,29 @@
 import React,{Component,Fragment} from 'react';
 
 class TodoList extends Component{
+    // constructor 在组件创建的第一个时刻自动被执行
     constructor(props){
         super(props);
+        // 在组件中创建了两个数据，数据一定要定义在state中
         this.state = {
             inputValue:'hello world',
             list:[]
         }
     }
 
+    handleInputChange(e){
+        this.setState({
+            inputValue:e.target.value
+        })
+    }
+
     render(){
         return(
             <Fragment>
-                <input value={this.state.inputValue}></input>
+                <input 
+                    value={this.state.inputValue}
+                    onChange={this.handleInputChange.bind(this)}
+                ></input>
                 <ul>
                     <li>learn React</li>
                     <li>learn Component</li>
